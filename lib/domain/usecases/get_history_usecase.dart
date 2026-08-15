03cfc64ed936dd60ae1c896938ecb6f7f0d57287
rizzxglithc <rshr4249@gmail.com>
2026-08-15 19:36:31 +0800

    fix: remove duplicate usecase declarations in get_history_usecase.dart

--- lib/domain/usecases/get_history_usecase.dart
+++ lib/domain/usecases/get_history_usecase.dart
@@ -10,27 +10,3 @@ class GetHistoryUseCase {
     return await repository.getHistory();
   }
 }
-
-class SaveHistoryUseCase {
-  final HistoryRepository repository;
-
-  SaveHistoryUseCase(this.repository);
-
-  Future<void> execute(DownloadItem item) async {
-    await repository.saveHistoryItem(item);
-  }
-}
-
-class DeleteHistoryUseCase {
-  final HistoryRepository repository;
-
-  DeleteHistoryUseCase(this.repository);
-
-  Future<void> execute(String id) async {
-    await repository.deleteHistoryItem(id);
-  }
-
-  Future<void> clearAll() async {
-    await repository.clearHistory();
-  }
-}
